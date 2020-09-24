0abde79a6b584e798b054f5a43a0d462562068b9
Chanwut Kittivorawong <chanwutk@gmail.com>
2020-09-24 00:20:28 -0700

    test + clean up

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,27 @@
-type GetID<Node, Id extends string | number> = (node: Node) => Id;
-
-export function toposort<Node, Id extends string | number>(
-  edges: [Node, Node][],
-  getId: GetID<Node, Id>,
-) {
-  const inDegrees = new Map<Id, number>();
-  const childrenList = new Map<Id, Set<Id>>();
+export function toposort<N extends string | number>(edges: [N, N][]) {
+  const indegree = new Map<N, number>();
+  const children = new Map<N, N[]>();
 
   edges.forEach(([parent, child]) => {
-    const parentId = getId(parent);
-    const childId = getId(child);
-    inDegrees.set(childId, (inDegrees.get(childId) ?? 0) + 1);
-    if (!childrenList.has(parentId)) childrenList.set(parentId, new Set());
-    childrenList.get(parentId)?.add(childId);
+    !indegree.has(parent) && indegree.set(parent, 0);
+    indegree.set(child, (indegree.get(child) ?? 0) + 1);
+    !children.has(parent) && children.set(parent, []);
+    children.get(parent)?.push(child);
   });
 
-  const queue: Id[] = [];
+  const queue: N[] = [];
   let queueHead = 0;
-  inDegrees.forEach((degree, id) => degree === 0 && queue.push(id));
+  indegree.forEach((degree, id) => degree === 0 && queue.push(id));
 
-  const ret: Id[] = [];
+  const ret: N[] = [];
   while (queue.length > queueHead) {
     const head = queue[queueHead++];
     ret.push(head);
 
-    const children = childrenList.get(head);
-    [...(children ?? [])]
-      .map((id) => (inDegrees.set(id, (inDegrees.get(id) ?? 0) - 1), id))
-      .filter((id) => inDegrees.get(id) === 0)
-      .forEach((id) => queue.push(id));
+    (children.get(head) ?? [])
+      .map(id => (indegree.set(id, (indegree.get(id) ?? 0) - 1), id))
+      .filter(id => indegree.get(id) === 0)
+      .forEach(id => queue.push(id));
   }
 
   return ret;
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,3 +1,24 @@
-import { toposort } from '../src/index';
+import {toposort} from '../src/index';
 
-test('empty', () => expect(toposort([], () => 0).length).toBe(0));
+test('empty', () => expect(toposort([]).length).toBe(0));
+
+function correctOrder(edges: [number, number][], result: number[]) {
+  edges.forEach(([parent, child]) =>
+    expect(
+      result.findIndex(d => d === parent) < result.findIndex(d => d === child),
+    ).toBeTruthy(),
+  );
+}
+
+test('small', () => {
+  const edges: [number, number][] = [
+    [5, 2],
+    [5, 0],
+    [4, 0],
+    [4, 1],
+    [2, 3],
+    [3, 1],
+  ];
+
+  correctOrder(edges, toposort(edges));
+});
